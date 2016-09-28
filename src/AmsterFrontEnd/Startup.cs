d241fce64ed8d12171d9531704d1772a5361051b
Supremo Furo Toro <ceriuswelshius@gmail.com>
2016-09-28 18:48:46 +0300

    t1

--- src/AmsterFrontEnd/Startup.cs
+++ src/AmsterFrontEnd/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.DependencyInjection;
 namespace AmsterFrontEnd
 {
     public class Startup
+
+
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
